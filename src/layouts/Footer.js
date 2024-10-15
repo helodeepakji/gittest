6b2f9c5a467fde9cfb3973bc80e5e9c8fee706fb
rimz05 <139325866+rimz05@users.noreply.github.com>
2024-10-15 10:39:46 +0530

    Update Footer.js
    added bootstrap footer by rimjhim

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -1,68 +1,88 @@
-import React from 'react';
-import './Footer.css';
-import  Logo from "../images/image.png"
+import React from "react";
 
-function Footer() {
+const Footer = () => {
   return (
-    <footer>
-      <div className="footer-container">
-        <div className="fig">
-          <img src={Logo} className="s6" alt="Logo" />
+    <footer className="bg-light py-5">
+      <div className="container">
+        <div className="row">
+        
+          <div className="col-md-2 mb-4">
+            <img src="/logo.png" alt="Logo" className="img-fluid mb-3" />
+          </div>
+
+       
+          <div className="col-md-2 mb-4">
+            <h4 className="text-secondary mb-3">PRODUCT</h4>
+            <ul className="list-unstyled">
+              <li><a href="#" className="text-muted text-decoration-none">Pricing</a></li>
+              <li><a href="#" className="text-muted text-decoration-none">Overview</a></li>
+              <li><a href="#" className="text-muted text-decoration-none">Browse</a></li>
+              <li><a href="#" className="text-muted text-decoration-none">Accessibility</a></li>
+              <li><a href="#" className="text-muted text-decoration-none">Five</a></li>
+            </ul>
+          </div>
+
+        
+          <div className="col-md-2 mb-4">
+            <h4 className="text-secondary mb-3">SOLUTION</h4>
+            <ul className="list-unstyled">
+              <li><a href="#" className="text-muted text-decoration-none">Brainstorming</a></li>
+              <li><a href="#" className="text-muted text-decoration-none">Ideation</a></li>
+              <li><a href="#" className="text-muted text-decoration-none">Wireframing</a></li>
+              <li><a href="#" className="text-muted text-decoration-none">Research</a></li>
+            </ul>
+          </div>
+
+        
+          <div className="col-md-2 mb-4">
+            <h4 className="text-secondary mb-3">RESOURCES</h4>
+            <ul className="list-unstyled">
+              <li><a href="#" className="text-muted text-decoration-none">Help Center</a></li>
+              <li><a href="#" className="text-muted text-decoration-none">Blog</a></li>
+              <li><a href="#" className="text-muted text-decoration-none">Tutorials</a></li>
+            </ul>
+          </div>
+
+         
+          <div className="col-md-2 mb-4">
+            <h4 className="text-secondary mb-3">Company</h4>
+            <ul className="list-unstyled">
+              <li><a href="#" className="text-muted text-decoration-none">About</a></li>
+              <li><a href="#" className="text-muted text-decoration-none">Press</a></li>
+              <li><a href="#" className="text-muted text-decoration-none">Events</a></li>
+              <li><a href="#" className="text-muted text-decoration-none">Careers</a></li>
+            </ul>
+          </div>
         </div>
-        <div className="footer-column">
-          <h2>Product</h2>
-          <p>Pricing</p>
-          <p>Overview</p>
-          <p>Browse</p>
-          <p>Accessibility</p>
-          <p>Five</p>
+
+        
+        <div className="d-flex justify-content-between mt-4">
+          <p className="text-muted">&copy; 2023 Protech, Inc. All rights reserved.</p>
+          <div className="d-flex gap-2">
+            <a href="#" className="text-muted text-decoration-none">
+              <i className="fab fa-facebook-f"></i>
+            </a>
+            <a href="#" className="text-muted text-decoration-none">
+              <i className="fab fa-twitter"></i>
+            </a>
+            <a href="#" className="text-muted text-decoration-none">
+              <i className="fab fa-instagram"></i>
+            </a>
+            <a href="#" className="text-muted text-decoration-none">
+              <i className="fab fa-linkedin"></i>
+            </a>
+          </div>
         </div>
-        <div className="footer-column">
-          <h2>
-            <i className="fa-solid fa-globe"></i> Solution
-            <i className="fa-solid fa-arrow-right" style={{ marginLeft: '10px' }}></i>
-          </h2>
-          <p>Brainstorming</p>
-          <p>Ideation</p>
-          <p>Wireframing</p>
-          <p>Research</p>
+
+       
+        <div className="mt-4">
+          <p className="text-muted small">
+            In sed posuere sed ullamcorper feugiat. Lacinia elit neque, ipsum, non. Tellus mattis enim volutpat habitasse. Semper posuere lectus consectetur aliquam et ullamcorper. Dictumst aenean justo fames diam eget volutpat vestibulum elit. Blandit aliquet bibendum pellentesque turpis id penatibus faucibus id nunc. Aenean rhoncus, erat pellentesque eu. Quis morbi condimentum phasellus in ultricies eu amet.
+          </p>
         </div>
-        <div className="footer-column">
-          <h2>Resources</h2>
-          <p>Help Center</p>
-          <p>Blog</p>
-          <p>Tutorials</p>
-        </div>
-        <div className="footer-column">
-          <h2>Company</h2>
-          <p>About</p>
-          <p>Solution</p>
-          <p>Events</p>
-          <p>Careers</p>
-          <p>Contact Us</p>
-        </div>
-      </div>
-      <hr />
-      <div>
-        <p className="footer">
-          @ 2023 Protech, Inc, All Rights Reserved
-          <span className="i">
-            <i className="fa-brands fa-youtube"></i>
-            <i className="fa-brands fa-facebook-f"></i>
-            <i className="fa-brands fa-twitter"></i>
-            <i className="fa-brands fa-instagram"></i>
-            <i className="fa-brands fa-linkedin-in"></i>
-          </span>
-        </p>
-        <p className="footer1">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
-          nostrum quasi consequuntur earum expedita voluptates quidem explicabo
-          odit suscipit doloribus, error quibusdam perferendis delectus vero
-          minus aspernatur. Ipsa, eligendi vel!
-        </p>
       </div>
     </footer>
   );
-}
+};
 
 export default Footer;
