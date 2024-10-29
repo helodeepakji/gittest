b93be616f093d5b577ab61ae587b04d12bad1c0c
Sonu <bhagyalaxmirout530@gmail.com>
2024-10-29 12:26:44 +0530

    contact page add

---
+++ src/layouts/Contactpage.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./Contactpage.css";
+import Nice from "./images/nice.jpg"
+
+const Contactpage = () => {
+  return (
+    <div className="section">
+      <div class="contact ">
+        <div class="my-id">
+          <h1>Contact Us</h1>
+          <input type="text" name="name" placeholder="Name" className="input" />
+          <br />
+          <input type="text" name="email" placeholder="Email" className="input" />
+          <br />
+          <input type="text" name="Phone" placeholder="Phone" className="input" />
+          <br />
+          <input type="text" name="Purpose" placeholder="Purpose" className="input" />
+          <br />
+          <button className="send">send</button>
+        </div>
+        <div class="one">
+          <img src={Nice} height="550px" width="570px" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contactpage;
--- src/layouts/Wallet.js
+++ src/layouts/Wallet.js
@@ -12,100 +12,96 @@ import { TiArrowBackOutline } from "react-icons/ti";
 import { IoIosArrowForward } from "react-icons/io";
 const Wallet = () => {
   return (
-    <div className="container all section ">  
-      <div className="main-div">
-      <div className="heading">
-        <h1>Designs</h1>
-        <div className="sideIMG">
-          <img src={topImg} alt="topImg" />
+    <div className="custom-container custom-all custom-section">  
+  <div className="custom-main-div">
+    <div className="custom-heading">
+      <h1>Designs</h1>
+      <div className="custom-side-img">
+        <img src={topImg} alt="topImg" />
+      </div>
+    </div>
+    <div className="custom-second-div">
+      <div className="custom-third-div">
+        <div className="custom-men">
+          <img src={menimg} alt="pic" />
+          <div>
+            <h3>Name of designer</h3>
+            <p>Project name</p>
+          </div>
+        </div>
+        <div className="custom-go-back">
+          <p>Go back</p>
+          <div className="custom-share">
+            <TiArrowBackOutline
+              style={{
+                color: "white",
+                backgroundColor: "red",
+                borderRadius: "5px",
+              }}
+            />
+          </div>
         </div>
       </div>
-      <div className="second-div">
-        <div className="third-div">
-          <div className="men">
-            <img src={menimg} alt="pic" />
-            <div>
-              <h3>Name of designer</h3>
-              <p>project name</p>
+
+      <div className="custom-fourth-div">
+        <div className="custom-image-section">
+          <img src={Cupimg} alt="pic" />
+        </div>
+        <div className="custom-chat-section">
+          <div className="custom-chat-header">
+            <div className="custom-chat-info">
+              <div className="custom-profile-picture">
+                <img src={menimg} alt="pic" />
+              </div>
+              <div className="custom-designer-name">
+                <h3>Name of designer</h3>
+              </div>
+            </div>
+            <div className="custom-calls-icon">
+              <IoCall fontSize={20} color="#0b5258" />
+              <IoVideocam fontSize={20} style={{ color: "#0b5258" }} />
             </div>
           </div>
-
-          <div className="goBack">
-            <p>Go back</p>
-            <div className="share">
-              <TiArrowBackOutline
-                style={{
-                  color: "white",
-                  backgroundColor: "red",
-                  borderRadius: "5px",
-                }}
+          <div className="custom-chat-msg">
+            <div className="custom-msg-receiver">
+              <p>Lorem ipsum is placeholder text commonly used in the graphic, print.</p>
+            </div>
+            <div className="custom-msg-sender">
+              <p>Hello there!</p>
+            </div>
+          </div>
+          <div className="custom-chat-footer">
+            <div className="custom-footer-icon">
+              <FaCamera fontSize={20} style={{ color: "#0b5258" }} />
+              <IoImageSharp fontSize={20} style={{ color: "#0b5258" }} />
+            </div>
+            <div className="custom-input-sec">
+              <input type="text" placeholder="message" />
+              <FaThumbsUp
+                className="custom-thumb"
+                fontSize={20}
+                style={{ color: "#0b5258" }}
               />
             </div>
           </div>
         </div>
-
-        <div className="fourth-div">
-          <div className="image-section">
-            <img src={Cupimg} alt="pic" />
-          </div>
-          <div className="chat-section">
-            <div className="chat-header">
-              <div className="chat-info">
-                <div className="profile-picture">
-                  <img src={menimg} alt="pic" />
-                </div>
-                <div className="desinger-name">
-                  <h3>Name of designer</h3>
-                </div>
-              </div>
-
-              <div className="calls-icon">
-                <IoCall fontSize={20} color="#0b5258" />
-                <IoVideocam fontSize={20} style={{ color: "#0b5258" }} />
-              </div>
-            </div>
-            <div className="chat-msg">
-              <div className="msg-reciever">
-                <p>
-                  Lorem ipsum is placeholder text commonly used in the graphic,
-                  print.
-                </p>
-              </div>
-              <div className="msg-sender">
-                <p>Hello there!</p>
-              </div>
-            </div>
-            <div className="chat-footer">
-              <div className="footer-icon">
-                <FaCamera fontSize={20} style={{ color: "#0b5258" }} />
-                <IoImageSharp fontSize={20} style={{ color: "#0b5258" }} />
-              </div>
-              <div className="input-sec">
-                <input type="text" placeholder="message" />
-                <FaThumbsUp
-                  className="thumb"
-                  fontSize={20}
-                  style={{ color: "#0b5258" }}
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="btnSection">
-          <button className="btn1">Accept Design</button>
-          <button className="btn2">Not Satisfied</button>
-        </div>
       </div>
-
-      <div className="next">
-        <h1>Next</h1>
-        <p className="arrow">
-          <IoIosArrowForward color="#0b5258" fontSize={25} />
-        </p>
+      
+      <div className="custom-btn-section">
+        <button className="custom-btn1">Accept Design</button>
+        <button className="custom-btn2">Not Satisfied</button>
       </div>
     </div>
+
+    <div className="custom-next">
+      <h1>Next</h1>
+      <p className="custom-arrow">
+        <IoIosArrowForward color="#0b5258" fontSize={25} />
+      </p>
     </div>
+  </div>
+</div>
+
    
   );
 };
