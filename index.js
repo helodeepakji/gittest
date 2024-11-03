e7276c83ae21553ce3af8f21a0933e589c28324f
helodeepakji <helodeepakji@gmail.com>
2024-11-03 12:42:13 +0530

    add backend

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
 const route = require('./router/route');
 
 app.use('/api/',route);
